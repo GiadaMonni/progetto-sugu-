ad6a24e9c55afbbb3ff46d5a7d6a175297525a9d
Giada Monni <gm.kyraraebrian@gmail.com>
2023-05-25 02:10:45 +0200

    continuo front-end card meravigliose

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,35 +24,35 @@
                                             <h2 class="mb-2"> € {{$announcement->price}}</h2>
                                             <p class="mb-4">{{$announcement->distretto}}</p>
                                             <p class="mb-1"><i class="uil uil-location-pin-alt size-22"></i></p>
-                                            <p class="mb-4">Drina, Serbia</p>
-                                            <a href="#0" class="link">Choose Date</a>
+                                            <p class="my-4"> Un esperienza da sogno </p>
+                                            <a href="#0" class="link"> Dettaglio </a>
                                             <div class="img-wrap img-2">
-                                                <img src="https://wordpress-network.prod.aws.skyscnr.com/wp-content/uploads/2019/01/Giappone.jpeg?w=1000&h=312&crop=1" alt="FIORI DI PESCO">
+                                                <img src="https://wordpress-network.prod.aws.skyscnr.com/wp-content/uploads/2019/01/Giappone.jpeg?w=1000&h=312&crop=1" alt="MONTE FUJI">
                                             </div>
                                             <div class="img-wrap img-1">
-                                                <img src="" alt="">
+                                                <img src="/img/tempio-giapu.png" alt="TEMPIO GIAPU">
                                             </div>
                                             <div class="img-wrap img-3">
-                                                <img src="https://assets.codepen.io/1462889/water.png" alt="">
+                                                <img src="/img/Senzanome.png" alt="">
                                             </div>
                                             <div class="img-wrap img-6">
-                                                <img src="https://assets.codepen.io/1462889/Stone.png" alt="">
+                                                <img src="/img/fioreLoto.png" alt="">
                                             </div>
                                         </div>
                                     </div>
                                     <div class="card-back">
                                         <div class="pricing-wrap">
-                                            <h4 class="mb-5">Camping</h4>
-                                            <h2 class="mb-2"><sup>$</sup>29 / 8<sup>hrs</sup></h2>
-                                            <p class="mb-4">per person</p>
+                                            <h4 class="mb-5"> Viaggio </h4>
+                                            <p class="mb-2"> Vieni a scoprire {{$announcement->name}}</p>
+                                            <p class="mb-4"> {{$announcement->body}} </p>
                                             <p class="mb-1"><i class="uil uil-location-pin-alt size-22"></i></p>
-                                            <p class="mb-4">Tara, Serbia</p>
-                                            <a href="#0" class="link">Choose Date</a>
+                                            {{-- <p class="mb-4">Tara, Serbia</p> --}}
+                                            <a href="#0" class="link"> dettaglio </a>
                                             <div class="img-wrap img-2">
-                                                <img src="https://assets.codepen.io/1462889/grass.png" alt="">
+                                                <img src="https://st.focusedcollection.com/9264440/i/650/focused_237840754-stock-photo-aerial-view-tokyo-night-cityscape.jpg" alt="">
                                             </div>
                                             <div class="img-wrap img-4">
-                                                <img src="https://assets.codepen.io/1462889/camp.png" alt="">
+                                                <img src="/img/gatto.png" alt="">
                                             </div>
                                             <div class="img-wrap img-5">
                                                 <img src="https://assets.codepen.io/1462889/Ivy.png" alt="">
